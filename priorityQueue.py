edae7031302b7228eb281a27986e2916bc9b19d9
josenelson2 <jose3202nelson@gmail.com>
2025-04-16 11:01:23 -0300

    Segunda Alteração

--- priorityQueue.py
+++ priorityQueue.py
@@ -2,5 +2,24 @@ from node import Node
 
 class priorityQueue:
     def __init__(self):
-        pass
-    pass
+        self.head = None #início da fila
+        self.tail = None #final da fila
+        self.sizeN = 0 #pessoas na fila normal
+        self.sizeP = 0 #pessoas na fila prioritária
+    
+
+
+    def list(self): #listar todas as pessoas da fila
+        current = self.head #current recebe o nó do início
+        while current: #enquanto  houver nós na fila
+            if current.priority:
+                tipo = "Prioridade"
+            else:
+                tipo = "Normal"
+            print(current.name + " - " + tipo) 
+            current = current.next
+
+    
+
+            
+
